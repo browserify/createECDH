96e6a1e18595b512ba600dd409640333d6fe30b5
Calvin Metcalf <cmetcalf@appgeo.com>
2015-01-21 08:11:56 -0500

    modularize

--- browser.js
+++ browser.js
@@ -1,10 +1,42 @@
 var elliptic = require('elliptic');
 var BN = require('bn.js');
 module.exports = ECDH;
-
-function ECDH(curve, crypto) {
-	elliptic.rand = crypto.randomBytes;
-	this.curve = new elliptic.ec(curve);
+var aliases = {
+	secp256k1: {
+		name: 'secp256k1',
+		byteLength: 32
+	},
+	secp224r1: {
+		name: 'p224',
+		byteLength: 28
+	},
+	prime256v1: {
+		name: 'p256',
+		byteLength: 32
+	},
+	prime192v1: {
+		name: 'p192',
+		byteLength: 24
+	},
+	ed25519: {
+		name: 'ed25519',
+		byteLength: 32
+	}
+};
+aliases.p224 = aliases.secp224r1;
+aliases.p256 = aliases.secp256r1 = aliases.prime256v1;
+aliases.p192 = aliases.secp192r1 = aliases.prime192v1;
+function ECDH(curve) {
+	if (!(this instanceof ECDH)) {
+		return new ECDH(curve);
+	}
+	this.curveType = aliases[curve];
+	if (!this.curveType ) {
+		this.curveType = {
+			name: curve
+		};
+	}
+	this.curve = new elliptic.ec(this.curveType.name);
 	this.keys = void 0;
 }
 ECDH.prototype.generateKeys = function (enc, format) {
@@ -21,7 +53,7 @@ ECDH.prototype.computeSecret = function (other, inenc, enc) {
 	other = other.toString(16);
 	var otherPub = this.curve.keyPair(other, 'hex').getPublic();
 	var out = otherPub.mul(this.keys.getPrivate()).getX();
-	return returnValue(out, enc);
+	return returnValue(out, enc, this.curveType.byteLength);
 };
 ECDH.prototype.getPublicKey = function (enc, format) {
 	var key = this.keys.getPublic(format === 'compressed', true);
@@ -56,11 +88,16 @@ ECDH.prototype.setPrivateKey = function (priv, enc) {
 	_priv = _priv.toString(16);
 	this.keys._importPrivate(_priv);
 };
-function returnValue(bn, enc) {
+function returnValue(bn, enc, len) {
 	if (!Array.isArray(bn)) {
 		bn = bn.toArray();
 	}
 	var buf = new Buffer(bn);
+	if (len && buf.length < len) {
+		var zeros = new Buffer(len - buf.length);
+		zeros.fill(0);
+		buf = Buffer.concat([zeros, buf]);
+	}
 	if (!enc) {
 		return buf;
 	} else {
